48e570302837312e390364998ce42e76a41a411d
devrsc <ramil.cobillajr@tup.edu.ph>
2022-11-09 23:25:47 +0800

    Reconfigured Tailwind Cli

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
+import MyFancyButton from "./src/components/MyFancyButton";
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-red-500'>
+    <View className='flex-1 items-center justify-center bg-white'>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style='auto' />
+      <MyFancyButton>What working na?</MyFancyButton>
     </View>
   );
 }
